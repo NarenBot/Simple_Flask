88465c225e1dc22db10c214b0b4bef3a202d285d
Naren <narendas10@gmail.com>
2023-03-28 19:05:27 +0530

    Added Misc_dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM python:3-alpine3.15
 COPY . /app
 WORKDIR /app
-# RUN pip install --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org --upgrade --proxy=127.0.0.1:3128 -r requirements.txt
 RUN pip install -r requirements.txt
 EXPOSE 8000
 CMD python ./app.py
--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template
-
+from redis import Redis
 
 app = Flask(__name__)
+# redis = Redis(host="redis", port=6379)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    # redis.incr("hits")
+    # counter = str(redis.get("hits"), "utf-8")
+    counter = 10
+    return render_template("index.html", counter=counter)
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
     # app.run(host="localhost", port=int("3000"), debug=True) [Not working]
-    # app.run(host="127.0.0.1", port=3000, debug=True) [Not working]
+    # app.run(host="127.0.0.1", port=3000, debug=True, use_reloader=True) [Not working]
     # app.run(host="192.168.1.5", port=8000, debug=True) [Container Exited]
